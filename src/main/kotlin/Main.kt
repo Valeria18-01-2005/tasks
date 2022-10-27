8bc956d1b699e319e7fa3b94e2cb694724c7ff31
Марина Жданова <zdanovalera95@gmail.com>
2022-10-27 21:00:53 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,341 @@
+                       // 1 тема
+                       //1 задание
+//import java.util.Scanner
+//fun main(){
+   // val input = Scanner(System.`in`)
+ //   val a = input.nextInt()
+   // val P = (4 * a)
+ //   println(P)
+//}
+//2 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val a = input.nextInt()
+//    val S = (a * a)
+ //   println(S)
+//}
+//3 задание
+
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System .`in`)
+ //   val a = input.nextInt()
+//    val b = input.nextInt()
+ //   val S = (a * b )
+ //   print("S=$S")
+ //   val P = 2*(a+b)
+//    println("\nP=$P")}
+//4 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val a = input.nextInt()
+//    val V = (a * a * a)
+//    print("V=$V")
+//    val S = (( 6 * a )*a)
+ //   println("\nS=$S")
+//}
+
+//5 задание
+
+//import java.util.Scanner
+//fun main(){
+ //   val input = Scanner(System.`in`)
+//    var A = input.nextInt()
+//    var B = input.nextInt()
+//    B = A.also{A = B}
+//    println(A)
+//    println(B)}
+//6 задание
+//fun main() {
+//    print("введите значение: ")
+//    val x = readLine()!!.toInt()
+//    val y = 3 * (x * x * x * x * x * x ) - 6 * (x * x) - 7
+//    println(y)
+//}
+//тема 2
+                       //1 задание
+
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val L= input.nextInt()
+//    print("Кол-во полных метров:${L/100}")}
+
+
+         //2 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val minuta= input.nextInt()
+//    print("Прошло полных минут:${minuta/60}")}
+                       //3 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val chislo= input.nextInt()
+//    print("Сумма цифр:${(chislo/10)+(chislo%10)}\nПроизведение цифр:${(chislo/10)*(chislo%10)}")
+//}
+
+                       //4 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val chislo= input.nextInt()
+//    print("Сумма цифр:${(chislo/100)+(chislo%10)+(chislo/10%10)}\nПроизведение цифр:${(chislo/100)*(chislo%10)*(chislo/10%10)}")
+//}
+
+                       //5 задание
+// import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val  a = input.nextInt()
+//    val b = (a/100)
+//    val c = (( a - b*100)/10)
+//    val d = (a%10)
+//    val new = ((c*100) + (d*10)+b)
+//    print(new)
+//}
+
+                       //3 тема
+                       //1 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B:Boolean = A >=A_1
+//    println(B)
+//}
+//const val A_1 = 0
+
+                       //2 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B:Boolean = (A%2!=0)
+//    println(B)
+//}
+// 3 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B:Boolean = (A%2==0)
+//    println(B)
+//}
+
+                       // 4 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B = input.nextInt()
+//    val C:Boolean = (A>2)&&(B<=3)
+//    println(C)
+//}
+                       //5задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B = input.nextInt()
+//    val C:Boolean = (A%2!=0)&&(B%2!=0)
+//    println(C)
+//}
+
+                       //6 задание
+//import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B = input.nextInt()
+//    val C = input.nextInt()
+//    val D:Boolean = (A>=0)&&(B>=0)&&(C>=0)
+//    println(D)
+//}
+                       //7 задание
+// import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System.`in`)
+//    val A = input.nextInt()
+//    val B = input.nextInt()
+//    val C = input.nextInt()
+//    val Dis = ((B*B)-(4*A*C))
+//    print("Значение дискриминанта:$Dis")
+//    val D :Boolean = Dis>=0
+//    println("\n$D")
+//
+//}
+                       //8 задание хз
+                       //4 тема
+                       //1 задание
+// import java.util.Scanner
+//fun main(){
+//    val input = Scanner(System .`in`)
+//    var a = input.nextInt()
+//    if (a >= 0 ){
+//    print(a+ 1 )}
+//    else{
+//        println(a)
+//    }
+//}
+                       // 2 задание
+// import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var a = input.nextInt()
+//    var b = input.nextInt()
+//    if (a > b ){
+//        print(a)
+//    }
+//    if(b > a ){
+//        print(b)
+//    }
+//
+//}
+                       //3 задание
+//import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var a = input.nextInt()
+//    var b = input.nextInt()
+//    var c = input.nextInt()
+//    if (a > b && a > c  ){
+//        print(a)
+//    }
+//    else if ( b > a && b > c){
+//        println(b)
+//    }
+//    else if ( c > a && c > b ){
+//        println(c)
+//    }
+//}
+                       //4 задача
+//import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var A = input.nextInt()
+//    var B = input.nextInt()
+//    if ( A != B ){
+//    if( A > B){
+//        B = A
+//        println(B)
+//    }else{
+//        B > A
+//        A = B
+//        println(A)
+//    }
+//        }
+//A == 0
+//B == 0
+//println(A)
+//println(B)}
+// 5 задание
+//import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var A = input.nextInt()
+//    var B = input.nextInt()
+//    if ( A >  0 && B > 0){
+//        print("Первая четверть")
+//    }
+//    else if (A < 0 && B > 0){
+//        print("Вторая четверть")
+//    }
+//    else if ( A < 0 && B <0 ){
+//        print("Третья четверть")
+//    }
+//    else{
+//        print("Четвертая четверть")
+//    }
+//}
+                       //6 задание
+//import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var x = input.nextInt()
+//    if ( x > 0 ){
+//        x = 2 * x
+//        print(x)
+//    };
+//    if ( x <= 0 ){
+//        x = 6 - x
+//        print(x)
+//    }
+//}
+                       //7 задание
+// import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var x = input.nextInt()
+//    if (x  <= 0 ){
+//        x = - x
+//        print(x)
+//    };
+//    if (x < 0 && x < 2){
+//        x = x*x
+//        print(x)
+//    };
+//    if (x >= 2 ){
+//        x = 4
+//        print(x)
+//
+//}}
+//                5 тема
+                       // 1 задание
+//import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var x = input.nextInt()
+//    x in 1..7
+//    when ( x){
+//        1 -> print("Понедельник")
+//        2 -> print("Вторник")
+//        3 -> print("Среда")
+//        4 -> print("Четверг")
+//        5 -> print("Пятница")
+//        6 -> print("Суббота")
+//        7 -> print("Воскресенье")
+//    }
+//}
+// 2 задание
+//import java.util.Scanner
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    var A = input.nextInt()
+//    var B = input.nextInt()
+//    var x = input.nextInt()
+//    when ( x){
+//        1 -> print(A+B)
+//        2 -> print(A - B )
+//        3 -> print( A * B)
+//        4 -> print( A / B)
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
